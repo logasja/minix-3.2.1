3e24cefd7f75a23b44ea7e0959f6831ee33bd64c
Paul Jacob Logas <Jakeman893@ufl.edu>
2016-09-14 16:06:29 -0400

    Update plog functions to match

--- servers/pm/plog.c
+++ servers/pm/plog.c
@@ -18,12 +18,12 @@ int get_plog_size()
 
 }
 
-int get_plog_byPID(int pid, long& c_time, long& t_time)
+int get_plog_byPID(int pid, long* c_time, long* t_time)
 {
 
 }
 
-int get_plog_byindex(int index, long& c_time, long& t_time)
+int get_plog_byindex(int index, long* c_time, long* t_time)
 {
 
 }
