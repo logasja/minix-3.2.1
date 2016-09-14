77330daf94e62e33d1702993eb35a83bcd126617
Paul Jacob Logas <Jakeman893@ufl.edu>
2016-09-14 14:57:24 -0400

    Update get_plog_byPID and get_plog_byIndex in proto.h

--- servers/pm/proto.h
+++ servers/pm/proto.h
@@ -105,9 +105,9 @@ int pm_isokendpt(int ep, int *proc);
 void tell_vfs(struct mproc *rmp, message *m_ptr);
 
 /* plog.c */
-(void) start_plog();
-(void) stop_plog();
-(void) reset_plog();
+void start_plog();
+void stop_plog();
+void reset_plog();
 int get_plog_size();
-int get_plog_byPID(int pid, long& c_time, long& t_time);
-int get_plog_byindex(int index, long& c_time, long& t_time);
+int get_plog_byPID(int pid, long* c_time, long* t_time);
+int get_plog_byindex(int index, long* c_time, long* t_time);
