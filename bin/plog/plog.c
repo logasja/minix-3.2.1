78c7559b3110d24a3adce4c6ac378760f17b54a9
Paul Jacob Logas <Jakeman893@ufl.edu>
2016-09-17 11:52:18 -0400

    Fix compilation errors

--- bin/plog/plog.c
+++ bin/plog/plog.c
@@ -20,17 +20,17 @@ int main(int argc, char *argv[])
 	int status = -1;
 	if (*argv)
 	{
-		if (strcmp(**argv, "start"))
+		if (strcmp(*argv, "start"))
 		{
 			status = start_plog();
 			fprintf(stderr, "Starting process logger.\n");
 		}
-		else if (strcmp(**argv, "stop"))
+		else if (strcmp(*argv, "stop"))
 		{
 			status = stop_plog();
 			fprintf(stderr, "Stopping process logger.\n");
 		}
-		else if (strcmp(**argv, "help"))
+		else if (strcmp(*argv, "help"))
 		{
 			fprintf(stderr, "usage: plog [start | stop | help] [-ipc] [index...process]\n");
 			status = EXIT_FAILURE;
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
 
 		if ((ch = getopt(argc, argv, "ipc")) != -1)
 		{
-			int param = atoi(**argv);
+			int param = atoi(*argv);
 			long start, end;
 			switch (ch) {
 			case 'p':
