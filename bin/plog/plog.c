651dde0bf1747ff3d482f84314da360b8a89569f
Paul Jacob Logas <Jakeman893@ufl.edu>
2016-09-17 11:57:36 -0400

    chagne for correct strcmp return

--- bin/plog/plog.c
+++ bin/plog/plog.c
@@ -20,22 +20,22 @@ int main(int argc, char *argv[])
 	int status = -1;
 	if (*argv)
 	{
-		if (strcmp(*argv, "start"))
+		if (!strcmp(*argv, "start"))
 		{
 			status = start_plog();
 			fprintf(stderr, "Starting process logger.\n");
 		}
-		else if (strcmp(*argv, "stop"))
+		else if (!strcmp(*argv, "stop"))
 		{
 			status = stop_plog();
 			fprintf(stderr, "Stopping process logger.\n");
 		}
-		else if (strcmp(*argv, "help"))
+		else if (!strcmp(*argv, "help"))
 		{
 			fprintf(stderr, "usage: plog [start | stop | help] [-ipc] [index...process]\n");
 			status = EXIT_FAILURE;
 		}
-		if (status < 0)
+		if (status >= 0)
 			exit(status);
 
 		int ch;
