161476452474f52a48c2f627b4994aa201b1a4b2
Paul Jacob Logas <Jakeman893@ufl.edu>
2016-09-17 21:48:03 -0400

    Fix kernel panic

--- servers/pm/plog.c
+++ servers/pm/plog.c
@@ -71,14 +71,14 @@ int log_start(int id)
 	if (!started)
 		return EXIT_FAILURE;
 
-	plog* tmp = (plog*)calloc(1,sizeof(plog));
-	tmp->p_id = id;
-	tmp->start_t = do_time();
-	tmp->end_t = -1;
-	buffer.arr[buffer.cur_index++] = tmp;
-	buffer.size += 1;
-	if (buffer.cur_index == PLOG_BUFFER_SIZE)
-		buffer.cur_index = 0;
+	//plog* tmp = (plog*)calloc(1,sizeof(plog));
+	//tmp->p_id = id;
+	//tmp->start_t = do_time();
+	//tmp->end_t = -1;
+	//buffer.arr[buffer.cur_index++] = tmp;
+	//buffer.size += 1;
+	//if (buffer.cur_index == PLOG_BUFFER_SIZE)
+	//	buffer.cur_index = 0;
 	return (EXIT_SUCCESS);
 }
 
