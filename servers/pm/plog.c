46eccf034627229015e403fa821edb8ef1cf53d6
Paul Jacob Logas <Jakeman893@ufl.edu>
2016-09-19 14:09:32 -0400

    Change debug message

--- servers/pm/plog.c
+++ servers/pm/plog.c
@@ -99,13 +99,13 @@ int log_end(int id)
 	if (started)
 	{
 		fprintf(stderr, "Looking for PID %d", id);
-		plog* tmp = find_by_PID(id);
-		fprintf(stderr, "Process %d retrieved.\n", tmp->p_id);
-		if (tmp)
+		plog* found = find_by_PID(id);
+		fprintf(stderr, "Process %d retrieved.\n", found->p_id);
+		if (found)
 		{
 			fprintf(stderr, "Process %d is ended.\n", id);
 			do_time();
-			tmp->end_t = mp->mp_reply.m2_l1;
+			found->end_t = mp->mp_reply.m2_l1;
 			fprintf(stderr, "Time ended is %ld", mp->mp_reply.m2_l1);
 			return EXIT_SUCCESS;
 		}
