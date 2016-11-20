c2d4dbc1fa8122deed4ef5d39e5c4159cf355bf1
Paul Jacob Logas <Jakeman893@ufl.edu>
2016-11-19 19:22:53 -0500

    Well that didn't work

--- servers/vfs/writelog.c
+++ servers/vfs/writelog.c
@@ -11,8 +11,7 @@ char* file_path = "/usr/tmp/log.txt";
 void writelog(char* buf)
 {
 	printf("\tOpening file.\n");
-	int handle = common_open(file_path, O_APPEND | O_CREAT, S_IWRITE);
-	//int handle = open(fp, O_APPEND | O_CREAT, S_IWRITE);
+	int handle = open(fp, O_APPEND | O_CREAT, S_IWRITE);
 	printf("\tFile open returned %d.\n", handle);
 	printf("\tWriting to file.\n");
 	write(handle, buf, strlen(buf));
