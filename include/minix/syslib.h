2669de3e34a59a4fc86b1a4e3836995aab5ef2f5
Jacob Logas <Jakeman893@ufl.edu>
2016-12-01 16:14:45 -0500

    Fix conflicting sys_statlog definitions

--- include/minix/syslib.h
+++ include/minix/syslib.h
@@ -41,7 +41,7 @@ int sys_schedule(endpoint_t proc_ep, int priority, int quantum, int
 int sys_schedctl(unsigned flags, endpoint_t proc_ep, int priority, int
 	quantum, int cpu);
 
-int sys_statlog(int funct, int pid, char* out);
+int sys_statlog(int funct, int pid, int index, char* out);
 
 /* Shorthands for sys_runctl() system call. */
 #define sys_stop(proc_ep) sys_runctl(proc_ep, RC_STOP, 0)
